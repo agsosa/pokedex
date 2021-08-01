eba95cff580c75e65c058d26b2ccb91bdcabeaec
Alejandro Sosa <agsosa74@gmail.com>
2021-07-31 21:23:06 -0300

    Add getPokemonsWithDetails & update api hook to use async await

--- src/lib/usePokedex.js
+++ src/lib/usePokedex.js
@@ -1,31 +1,70 @@
 import axios from 'axios';
 
 export default function usePokedex() {
-  const getPokemons = () => {
+  const getPokemons = async () => {
     const URL = 'https://pokeapi.co/api/v2/pokemon';
 
-    return new Promise((resolve) =>
-      axios
-        .get(URL)
-        .then(({ data }) => resolve({ error: false, data: data.results }))
-        .catch((error) => resolve({ error, data: null }))
-    );
+    try {
+      const { data } = await axios.get(URL);
+      return { error: false, data: data.results };
+    } catch (error) {
+      return { error, data: null };
+    }
   };
-  1;
 
-  const getPokemonDetails = (id_or_name) => {
+  const getPokemonDetails = async (id_or_name) => {
     const URL = `https://pokeapi.co/api/v2/pokemon/${id_or_name}`;
 
-    return new Promise((resolve) =>
-      axios
-        .get(URL)
-        .then(({ data }) => resolve({ error: false, data }))
-        .catch((error) => resolve({ error, data: null }))
-    );
+    try {
+      const { data } = await axios.get(URL);
+
+      const resultData = {
+        id: data.id,
+        name: data.name,
+        sprite: data.sprites?.other?.dream_world?.front_default,
+        stats: {
+          health: data.stats[0]?.base_stat,
+          attack: data.stats[1]?.base_stat,
+          defense: data.stats[2]?.base_stat,
+        },
+        types: data.types.map((t) => t.type.name),
+      };
+
+      return { error: false, data: resultData };
+    } catch (error) {
+      return { error, data: null };
+    }
+  };
+
+  const getPokemonsWithDetails = async () => {
+    const promises = [];
+    const result = [];
+
+    // Get pokemons list
+    const list = await getPokemons();
+    if (list.error) return list;
+
+    for (let pokemon of list.data) {
+      // For each pokemon in the list, fetch the details and save the promise in our promises array
+      promises.push(
+        getPokemonDetails(pokemon.name).then((details) => {
+          if (details.error) return details;
+
+          result.push(details.data); // Save the details in our result array
+        })
+      );
+    }
+
+    // Wait for all the pokemon details promises
+    await Promise.allSettled(promises);
+
+    // Resolve
+    return { error: false, data: result };
   };
 
   return {
     getPokemons,
     getPokemonDetails,
+    getPokemonsWithDetails,
   };
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,18 @@
 import '@/styles/globals.css';
 import GlobalStyles from '@/components/misc/GlobalStyles';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Pokedex</title>
+        <meta name='description' content='Pokedex App' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
       <GlobalStyles />
+
       <Component {...pageProps} />
     </>
   );
