07c33208d7217eae400551b3238bb9c35cb3ca6a
Alejandro Sosa <agsosa74@gmail.com>
2021-08-01 02:14:27 -0300

    Disable eslint on builds

--- next.config.js
+++ next.config.js
@@ -8,4 +8,9 @@ module.exports = {
 
     return config
   },
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
