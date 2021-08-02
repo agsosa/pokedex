b291fec9f0715c7cda4e29f7082bc65b52dfeae8
Alejandro Sosa <agsosa74@gmail.com>
2021-08-01 23:08:55 -0300

    Add translated pokemon types

--- src/components/pokemon/common/TypesList.js
+++ src/components/pokemon/common/TypesList.js
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import tw, { styled } from 'twin.macro';
 import TypeTag from './TypeTag';
+import useTranslation from 'next-translate/useTranslation';
 
 const TypesContainer = styled.div(({ big }) => [tw`flex space-x-2`, big && tw`text-3xl`]);
 
 export default function TypesList({ pokemon, big = false }) {
+  const { t } = useTranslation('pokemon_types');
+
   return (
     <TypesContainer big={big}>
-      {pokemon.types.map((t) => (
-        <TypeTag type={t.type.name} />
+      {pokemon.types.map(({ type }) => (
+        <TypeTag type={t(type?.name?.toLowerCase() || "unknown")} />
       ))}
     </TypesContainer>
   );
