0501b2ca11965d76e588c51303e49ea3dedb7477
Alejandro Sosa <agsosa74@gmail.com>
2021-08-01 21:33:47 -0300

    Update details page responsive styles

--- src/components/pokemon/common/AbilitiesList.js
+++ src/components/pokemon/common/AbilitiesList.js
@@ -1,7 +1,7 @@
 import tw from 'twin.macro';
 import AbilityCard from './AbilityCard';
 
-const AbilitiesContainer = tw.div`w-full flex flex-col space-y-6 max-w-lg`;
+const AbilitiesContainer = tw.div`w-full flex flex-col space-y-6 max-w-lg text-center md:text-left`;
 
 export default function AbilitiesList({ pokemon }) {
   return (
