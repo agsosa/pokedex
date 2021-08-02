59b6bb330ec634203675cb86c4fb94506641cb1c
Alejandro Sosa <agsosa74@gmail.com>
2021-08-01 22:11:17 -0300

    Add loading bar & fix ability description error

--- src/components/pokemon/common/AbilitiesList.js
+++ src/components/pokemon/common/AbilitiesList.js
@@ -4,6 +4,8 @@ import AbilityCard from './AbilityCard';
 const AbilitiesContainer = tw.div`w-full flex flex-col space-y-6 max-w-lg text-center md:text-left`;
 
 export default function AbilitiesList({ pokemon }) {
+  if (!Array.isArray(pokemon.abilitiesData) || pokemon.abilitiesData.length === 0) return "No abilities";
+
   return (
     <AbilitiesContainer>
       {pokemon.abilitiesData.map((a) => (
